7a612930059d8306fd82831e310e834d23e7c245
Deric Loo <deric@cantonkitchen.com.my>
2026-06-24 14:16:50 +0800

    Remove self redirect loop

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "laozhaopai-32-worldcup-game.vercel.app",
-          },
-        ],
-        destination: "https://games.brainwaveai.my/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
