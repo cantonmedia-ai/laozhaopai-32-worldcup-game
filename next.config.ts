33ad33072a5e92730b366dbf547f64b70ec7061e
Deric Loo <deric@cantonkitchen.com.my>
2026-06-24 14:10:41 +0800

    Redirect old Vercel URL to live domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "laozhaopai-32-worldcup-game.vercel.app",
+          },
+        ],
+        destination: "https://games.brainwaveai.my/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
